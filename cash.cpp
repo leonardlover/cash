02634e6d3ac351f5bf9386d6a27e1dac9e24c04a
Ana <anvillagran2021@inf.udec.cl>
2024-08-28 19:45:34 -0400

    Commands are executed by children; pipes need more work

--- cash.cpp
+++ cash.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unistd.h> 
+#include <sys/wait.h>
 
 int main(void)
 {
@@ -35,7 +37,51 @@ int main(void)
             }
             std::cout << std::endl;
         }
+        
+        int num_children = commands.size();
+        pid_t pid;
+        int pipes[2];
+        pipe(pipes);
 
+        for(int i=0; i<num_children; i++){
+            pid = fork();
+
+            if(pid == -1){
+                std::cout << "Error en la creación del proceso hijo " << i << std::endl;
+                exit(1);
+            }
+            else if(pid == 0){
+
+                if(i%2 == 0){
+                    // Resultado es escrito en pipe
+                    dup2(pipes[1], 1);
+                }
+                else{
+                    // Entrada es extraída de pipe1
+                    dup2(pipes[0], 0);
+                }
+
+                close(pipes[0]);
+                close(pipes[1]);
+
+                int size = commands[0].size();
+                char *args[size + 1];
+                args[size] = NULL;
+
+                for(int j=0; j<size; j++){
+                    args[j] = commands[i][j].data();
+                }
+
+                execvp(*args, args);
+            }
+        }
+
+        close(pipes[0]);
+        close(pipes[1]);
+
+        for(int i=0; i<num_children; i++){
+            wait(NULL);
+        }
     }
 
     return 0;
