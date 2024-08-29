b8f4377055310c304a731e32d3167db52bbddb6f
Ana <anvillagran2021@inf.udec.cl>
2024-08-28 23:39:47 -0400

    Pipes are working

--- cash.cpp
+++ cash.cpp
@@ -30,6 +30,7 @@ int main(void)
             }
         }
 
+        /*
         for (int i = 0; i < commands.size(); i++) {
             std::cout << commands[i].size() << std::endl;
             for (int j = 0; j < commands[i].size(); j++) {
@@ -37,47 +38,83 @@ int main(void)
             }
             std::cout << std::endl;
         }
+        */
         
         int num_children = commands.size();
         pid_t pid;
-        int pipes[2];
-        pipe(pipes);
 
-        for(int i=0; i<num_children; i++){
+        if(num_children == 1){
             pid = fork();
 
             if(pid == -1){
-                std::cout << "Error en la creación del proceso hijo " << i << std::endl;
+                std::cout << "Error en la creación del proceso hijo" << std::endl;
                 exit(1);
             }
+
             else if(pid == 0){
-
-                if(i%2 == 0){
-                    // Resultado es escrito en pipe
-                    dup2(pipes[1], 1);
-                }
-                else{
-                    // Entrada es extraída de pipe1
-                    dup2(pipes[0], 0);
-                }
-
-                close(pipes[0]);
-                close(pipes[1]);
-
                 int size = commands[0].size();
                 char *args[size + 1];
                 args[size] = NULL;
 
                 for(int j=0; j<size; j++){
-                    args[j] = commands[i][j].data();
+                    args[j] = commands[0][j].data();
                 }
 
-                execvp(*args, args);
+                execvp(*args, args);          
             }
         }
 
-        close(pipes[0]);
-        close(pipes[1]);
+        // Múltiples hijos requieren pipes
+        else if(num_children > 1){
+            int pipes[2*(num_children - 1)];
+            for(int i=0; i<num_children - 1; i++){
+                pipe(pipes + 2*i);
+            }
+
+            for(int i=0; i<num_children; i++){
+                pid = fork();
+
+                if(pid == -1){
+                    std::cout << "Error en la creación del proceso hijo " << i << std::endl;
+                    exit(1);
+                }
+                else if(pid == 0){
+
+                    if(i == 0){
+                        // Primer proceso recibe input por entrada estándar o argumento y escribe en primer pipe
+                        dup2(pipes[1], 1);
+                    }
+                    else if(i == num_children - 1){
+                        // Último proceso recibe entrada por último pipe y escribe en salida estándar (si es que escribe)
+                        dup2(pipes[2*i - 2], 0);
+                    }
+                    else{
+                        // Cualquier otro proceso intermedio recibe entrada por pipe anterior y escribe en pipe siguiente
+                        dup2(pipes[2*i - 2], 0);
+                        dup2(pipes[2*i + 1], 1);
+                    }
+
+                    for(int j=0; j<2*(num_children - 1); j++){
+                        close(pipes[j]);
+                    }
+
+                    int size = commands[0].size();
+                    char *args[size + 1];
+                    args[size] = NULL;
+
+                    for(int j=0; j<size; j++){
+                        args[j] = commands[i][j].data();
+                    }
+
+                    execvp(*args, args);
+                }
+
+            }
+
+            for(int j=0; j<2*(num_children - 1); j++){
+                close(pipes[j]);
+            }
+        }
 
         for(int i=0; i<num_children; i++){
             wait(NULL);
