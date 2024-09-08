525e6538f9567cca8182cc4656149e1cc284f7e3
Leonardo Lovera <leonard.lover@gmail.com>
2024-09-08 03:35:38 -0300

    feat: added lexer, still need to work on punctuation

---
+++ lexer.cpp
@@ -0,0 +1,205 @@
+#include <deque>
+#include <sstream>
+#include <string>
+
+#include <iostream>
+using namespace std;
+
+class lexer
+{
+public:
+//private:
+    std::stringstream instream;
+    bool posix;
+    std::string commenters;
+    std::string wordchars;
+    std::string whitespace;
+    //bool whitespace_split;
+    std::string quotes;
+    std::string escape;
+    std::string escapedquotes;
+    std::string state;
+    std::deque<char> pushback;
+    std::string token;
+
+    std::string get_token(void)
+    {
+        if (!pushback.empty()) {
+            std::string tok = "";
+            tok += pushback.front();
+            pushback.pop_front();
+            return tok;
+        }
+
+        bool quoted = false;
+        std::string escapedstate = " ";
+        char nextchar;
+        bool nextchar_eof;
+        std::string comment;
+
+        while (true) {
+            nextchar_eof = instream.get(nextchar) ? false : true;
+
+            std::cout << "state is '" << state << "' and nextchar is '" << nextchar << "'" << std::endl;
+            std::cout << "token is " << token << std::endl;
+
+            if (state == "") {
+                token = "";
+                break;
+            } else if (state == " ") {
+                if (nextchar_eof) {
+                    state = "";
+                    break;
+                } else if (whitespace.find(nextchar) != std::string::npos) {
+                    if (token != "" || (posix && quoted)) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else if (commenters.find(nextchar) != std::string::npos) {
+                    std::getline(instream, comment);
+                } else if (posix && escape.find(nextchar) != std::string::npos) {
+                    escapedstate = "a";
+                    state = nextchar;
+                } else if (wordchars.find(nextchar) != std::string::npos) {
+                    token = nextchar;
+                    state = "a";
+                } else if (quotes.find(nextchar) != std::string::npos) {
+                    if (!posix)
+                        token = nextchar;
+                    state = nextchar;
+                } else {
+                    token = nextchar;
+                    state = "a";
+                }
+                    /*
+                } else if (whitespace_split) {
+                    token = nextchar;
+                    state = "a";
+                } else {
+                    token = nextchar;
+                    if (token != "" || (posix && quoted)) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                */
+            } else if (quotes.find(state) != std::string::npos) {
+                quoted = true;
+                if (nextchar_eof) {
+                    throw; // jiehuehufrhfurhfurhuhf
+                }
+
+                if (state.length() == 1 && nextchar == state.front()) {
+                    if (!posix) {
+                        token += nextchar;
+                        state = " ";
+                        break;
+                    } else {
+                        state = "a";
+                    }
+                } else if (posix && escape.find(nextchar) != std::string::npos &&
+                           escapedquotes.find(state) != std::string::npos) {
+                    escapedstate = state;
+                    state = nextchar;
+                } else {
+                    token += nextchar;
+                }
+            } else if (escape.find(state) != std::string::npos) {
+                if (nextchar_eof) {
+                    throw; //huehuheuir
+                }
+
+                if (quotes.find(escapedstate) != std::string::npos &&
+                    (state.length() != 1 || nextchar != state.front()) &&
+                    (escapedstate.length() != 1 || nextchar != escapedstate.front())) {
+                    token += state;
+                }
+
+                token += nextchar;
+                state = escapedstate;
+            } else if (state == "a") {
+                if (nextchar_eof) {
+                    state = "";
+                    break;
+                } else if (whitespace.find(nextchar) != std::string::npos) {
+                    state = " ";
+                    if (token != "" || (posix && quoted)) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else if (commenters.find(nextchar) != std::string::npos) {
+                    std::getline(instream, comment);
+                    if (posix) {
+                        state = " ";
+                        if (token != "" || (posix && quoted)) {
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                } else if (posix && quotes.find(nextchar) != std::string::npos) {
+                    state = nextchar;
+                } else if (posix && escape.find(nextchar) != std::string::npos) {
+                    escapedstate = "a";
+                    state = nextchar;
+                } else if (wordchars.find(nextchar) != std::string::npos ||
+                           quotes.find(nextchar) != std::string::npos) {
+                    token += nextchar;
+                } else {
+                    pushback.push_front(nextchar);
+                    state = " ";
+                    if (token != "" || (posix && quoted)) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        std::string result = token;
+        token = "";
+        return result;
+    }
+
+    lexer(std::string in, bool px): posix(px)
+    {
+        instream.str(in);
+        commenters = "#";
+        wordchars = "abcdfeghijklmnopqrstuvwxyz";
+        wordchars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+
+        /*
+        if (px) {
+            wordchars += "ßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ";
+            wordchars += "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞ";
+        }
+        */
+
+        whitespace = " \t\r\n";
+        //whitespace_split = true;
+        quotes = "\'\"";
+        escape = "\\";
+        escapedquotes = "\"";
+        state = " ";
+        token = "";
+    }
+};
+
+int main(void)
+{
+    lexer lex("cat test.txt", true);
+    //lex.whitespace_split = true;
+
+    while (true) {
+        std::string tok = lex.get_token();
+        if (tok == "")
+            break;
+        cout << tok << endl;
+    }
+
+    return 0;
+}
